7e8ed11ff1d5095e301733962cb3ef79e0f8833e
Gonzalez-Ignacio <ignaciogonzalez2306@gmail.com>
2023-07-08 18:59:50 -0300

    Pre-Finalizado
    Falta agregar animaciones y foto

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Sección Scroll
+let seccion = document.querySelectorAll("section");
+let navLink = document.querySelectorAll("nav div div ul li a");
+
+
+window.onscroll = () => {
+  seccion.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height) {
+      // active de navbar link
+      navLink.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector("nav div div ul li a[href*=" + id + "]").classList.add('active');
+      })
+    }
+  });
+  // header
+  let navbar = document.querySelector('.navbar');
+  navbar.classList.toggle("sticky", window.scrollY > 100);
+}
+
